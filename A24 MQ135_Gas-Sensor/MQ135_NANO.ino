8ea5a0e6537b82f0fd60401fceaf53802c2eb543
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-10-25 18:42:29 +0100

    Add files via upload

---
+++ A24 MQ135_Gas-Sensor/MQ135_NANO.ino
@@ -0,0 +1,38 @@
+  
+/*
+Board: NANO
+ATMega328P Old Bootloader
+
+KHF 25.10.2020
+
+Seriell: 9600 Baud 
+
+18:36:03.111 -> CO2 ppm value : 0.18
+18:36:04.089 -> CO2 ppm value : 0.17
+18:36:05.106 -> CO2 ppm value : 0.18
+18:36:06.089 -> CO2 ppm value : 0.20
+
+
+*/
+
+#include "MQ135.h"
+#define ANALOGPIN A0    //  Define Analog PIN on Arduino Board
+#define RZERO 206.85    //  Define RZERO Calibration Value
+MQ135 gasSensor = MQ135(ANALOGPIN);
+
+void setup()
+{
+  Serial.begin(9600);
+  float rzero = gasSensor.getRZero();
+  delay(3000);
+  Serial.print("MQ135 RZERO Calibration Value : ");
+  Serial.println(rzero);
+}
+
+void loop() {
+  float ppm = gasSensor.getPPM();
+  delay(1000);
+  digitalWrite(13,HIGH);
+  Serial.print("CO2 ppm value : ");
+  Serial.println(ppm);
+}
