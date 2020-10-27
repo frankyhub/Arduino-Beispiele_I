6787e2d14cb31e3dc77c1702d2d175521e51792b
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-10-27 16:47:23 +0100

    Add files via upload

---
+++ A19 Dashboard/Cayenne_Buero_V1/Cayenne_Buero_V1.ino
@@ -0,0 +1,72 @@
+/*ESP8266 mit DHT11 Sensor und Relais gesteuert über MQTT Dashboard Cayenne
+ * https://cayenne.mydevices.com/ 
+ * Board: WeMos D1 R2 & mini
+ * Uploadspeed: 921600
+ * CPU Freq: 80MHz
+ * Flash Size 4M
+ * 
+ * KHF 23.10.2020
+ */
+
+ 
+//#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP8266.h>
+#include <DHTesp.h>
+
+// WiFi network info.
+#define SSID "R2-D2"
+#define WIFIPASSWORD "2QK384JVPX"
+
+// Cayenne Zugangsdaten. Diese erhalten wir vom Cayenne Dashboard.
+#define USERNAME "268e68e0-0d3f-11eb-883c-638d8ce4c23d"
+#define PASSWORD "f359cce71821bb9b7824527bee3d484f4f824e73"
+#define CLIENTID "9887e3c0-1868-11eb-a2e4-b32ea624e442"
+
+//Kanäle zur Kommunikation mit dem Dashboard
+#define KANAL_TEMPERATUR 1
+#define KANAL_FEUCHTE 2
+#define KANAL_RELAIS 3
+#define KANAL_STATUS 4
+
+#define PIN_DHT 5
+#define PIN_RELAIS 4
+
+//globale Variablen
+boolean relais_status = 0;
+TempAndHumidity neueWerte;
+DHTesp dht;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_RELAIS,OUTPUT);
+  digitalWrite(PIN_RELAIS,relais_status);
+  dht.setup(PIN_DHT, DHTesp::DHT11);
+  Cayenne.begin(USERNAME, PASSWORD, CLIENTID, SSID, WIFIPASSWORD);
+}
+
+void loop() {
+  Cayenne.loop();
+}
+
+// Diese Funktion wird von Cayenne.loop in regelmäßigen Abständen aufgerufen
+// Hier sollten die Daten der Sensoren übertragen werden
+CAYENNE_OUT_DEFAULT()
+{
+  neueWerte = dht.getTempAndHumidity();
+  if (dht.getStatus() == 0) {
+    Cayenne.celsiusWrite(KANAL_TEMPERATUR,neueWerte.temperature);
+    Cayenne.virtualWrite(KANAL_FEUCHTE,neueWerte.humidity,TYPE_RELATIVE_HUMIDITY,UNIT_PERCENT);
+  }
+  Cayenne.digitalSensorWrite(KANAL_STATUS,relais_status);
+}
+
+//Diese Funktion wird von Cayenne.loop aufgerufen wenn Daten für Aktoren von Cayenne anstehen
+//Hier sollte auf diese Daten reagiert werden
+CAYENNE_IN_DEFAULT()
+{
+  if (request.channel == KANAL_RELAIS) {
+    relais_status = getValue.asInt();
+    digitalWrite(PIN_RELAIS,relais_status);
+  }
+}
---
+++ A19 Dashboard/Cayenne_Buero_oZd_V1/Cayenne_Buero_oZd_V1.ino
@@ -0,0 +1,72 @@
+/*ESP8266 mit DHT11 Sensor und Relais gesteuert über MQTT Dashboard Cayenne
+ * https://cayenne.mydevices.com/ 
+ * Board: WeMos D1 R2 & mini
+ * Uploadspeed: 921600
+ * CPU Freq: 80MHz
+ * Flash Size 4M
+ * 
+ * KHF 23.10.2020
+ */
+
+ 
+//#define CAYENNE_DEBUG
+#define CAYENNE_PRINT Serial
+#include <CayenneMQTTESP8266.h>
+#include <DHTesp.h>
+
+// WiFi network info.
+#define SSID "x"
+#define WIFIPASSWORD "x"
+
+// Cayenne Zugangsdaten. Diese erhalten wir vom Cayenne Dashboard.
+#define USERNAME "x"
+#define PASSWORD "x"
+#define CLIENTID "x"
+
+//Kanäle zur Kommunikation mit dem Dashboard
+#define KANAL_TEMPERATUR 1
+#define KANAL_FEUCHTE 2
+#define KANAL_RELAIS 3
+#define KANAL_STATUS 4
+
+#define PIN_DHT 5
+#define PIN_RELAIS 4
+
+//globale Variablen
+boolean relais_status = 0;
+TempAndHumidity neueWerte;
+DHTesp dht;
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(PIN_RELAIS,OUTPUT);
+  digitalWrite(PIN_RELAIS,relais_status);
+  dht.setup(PIN_DHT, DHTesp::DHT11);
+  Cayenne.begin(USERNAME, PASSWORD, CLIENTID, SSID, WIFIPASSWORD);
+}
+
+void loop() {
+  Cayenne.loop();
+}
+
+// Diese Funktion wird von Cayenne.loop in regelmäßigen Abständen aufgerufen
+// Hier sollten die Daten der Sensoren übertragen werden
+CAYENNE_OUT_DEFAULT()
+{
+  neueWerte = dht.getTempAndHumidity();
+  if (dht.getStatus() == 0) {
+    Cayenne.celsiusWrite(KANAL_TEMPERATUR,neueWerte.temperature);
+    Cayenne.virtualWrite(KANAL_FEUCHTE,neueWerte.humidity,TYPE_RELATIVE_HUMIDITY,UNIT_PERCENT);
+  }
+  Cayenne.digitalSensorWrite(KANAL_STATUS,relais_status);
+}
+
+//Diese Funktion wird von Cayenne.loop aufgerufen wenn Daten für Aktoren von Cayenne anstehen
+//Hier sollte auf diese Daten reagiert werden
+CAYENNE_IN_DEFAULT()
+{
+  if (request.channel == KANAL_RELAIS) {
+    relais_status = getValue.asInt();
+    digitalWrite(PIN_RELAIS,relais_status);
+  }
+}
