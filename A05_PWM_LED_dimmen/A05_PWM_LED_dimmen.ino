63af79d6c6c0515a7f203203ae53473e076ed70d
frankyhub <61152841+frankyhub@users.noreply.github.com>
2020-11-22 07:39:43 +0100

    Update A05_PWM_LED_dimmen.ino

--- A05_PWM_LED_dimmen/A05_PWM_LED_dimmen.ino
+++ A05_PWM_LED_dimmen/A05_PWM_LED_dimmen.ino
@@ -3,27 +3,40 @@
  * Board: Uno
  */
 
-int LED = 6;
-int REGLER = A0;
-int ReglerWert;
+/*
+ * LED dimmen
+ * Board: Uno
+ */
+
+int LED1 = 6;
+int LED3 = 4;
+
+int REGLER1 = A0;
+int REGLER2 = A1;
+
+int ReglerWert1;
+int ReglerWert2;
 
 void setup()
 {
-pinMode(LED, OUTPUT);
+pinMode(LED1, OUTPUT);
+pinMode(LED3, OUTPUT); 
+  
 // Seriellen Monitor starten
 Serial.begin(9600);
 }
-void loop()
-/*
-Alternativ kannst du auch den map-Befehl verwenden: 
-{
-Serial.print(ReglerWert);
-ReglerWert = analogRead(REGLER);
-analogWrite(LED, ReglerWert / 4);
-}
-*/
 
+void loop()
 {
-ReglerWert = analogRead(REGLER);
-analogWrite (LED, map(ReglerWert, 0, 1023, 0, 255));
+//LED mit Analogeingang dimmen  
+ReglerWert1 = analogRead(REGLER1);
+analogWrite (LED1, map(ReglerWert1, 0, 1023, 0, 255));
+
+//LED mit Analogeingang e/a schalten  
+ReglerWert2 = analogRead(REGLER2);
+if (ReglerWert2>700) digitalWrite(LED3, HIGH);
+if (ReglerWert2<400) digitalWrite(LED3, LOW);
+    Serial.print(ReglerWert2);
+    Serial.println(); 
+    delay(100);
 }
