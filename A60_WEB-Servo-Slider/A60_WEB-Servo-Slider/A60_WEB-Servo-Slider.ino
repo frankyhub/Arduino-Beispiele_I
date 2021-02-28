b2cd42534a4d6021ec727434495fd0fb5cdab703
frankyhub <61152841+frankyhub@users.noreply.github.com>
2021-02-28 10:21:23 +0100

    Add files via upload

---
+++ A60_WEB-Servo-Slider/A60_WEB-Servo-Slider/A60_WEB-Servo-Slider.ino
@@ -0,0 +1,146 @@
+/* 
+
+PROGRAMMINFO
+
+Funktion: ESP32 WEB Server zur Servo-Steuerung 
+
+Version: 28.02.2021
+
+(x) C++ Arduino IDE V1.8.13
+
+Board: ESP36 Dev Module
+
+Einstellungen:
+https://dl.espressif.com/dl/package_esp32_index.json
+http://dan.drown.org/stm32duino/package_STM32duino_index.json
+https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_dev_index.json
+
+
+Librarys
+- WiFi.h V0.16.1
+- Servo  https://github.com/RoboticsBrno/ServoESP32
+servoPin = 13; 
+*********/
+
+#include <WiFi.h>
+#include <Servo.h>
+
+Servo myservo;  
+
+static const int servoPin = 13;
+
+//PASSWORT:
+const char* ssid     = "xxx";
+const char* password = "xxx";
+
+WiFiServer server(80);
+
+String header;
+
+String valueString = String(5);
+int pos1 = 0;
+int pos2 = 0;
+
+unsigned long currentTime = millis();
+
+unsigned long previousTime = 0; 
+// 2000ms = 2s)
+const long timeoutTime = 2000;
+
+void setup() {
+  Serial.begin(115200);
+
+  myservo.attach(servoPin);  
+
+  
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.begin();
+}
+
+void loop(){
+  WiFiClient client = server.available();   
+
+  if (client) {                             
+    currentTime = millis();
+    previousTime = currentTime;
+    Serial.println("New Client.");          
+    String currentLine = "";                
+    while (client.connected() && currentTime - previousTime <= timeoutTime) { 
+      currentTime = millis();
+      if (client.available()) {             
+        char c = client.read();             
+        Serial.write(c);                    
+        header += c;
+        if (c == '\n') {                  
+          if (currentLine.length() == 0) {
+            
+            e:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+
+            // HTML 
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>body { text-align: center; font-family: \"Trebuchet MS\", Arial; margin-left:auto; margin-right:auto;}");
+            client.println(".slider { width: 300px; }</style>");
+            client.println("<script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script>");
+                     
+            // Web Page
+            client.println("</head><body><h1>ESP32 Servo-Steuerung</h1>");
+            client.println("<p>Position: <span id=\"servoPos\"></span></p>");          
+            client.println("<input type=\"range\" min=\"0\" max=\"180\" class=\"slider\" id=\"servoSlider\" onchange=\"servo(this.value)\" value=\""+valueString+"\"/>");
+            
+            client.println("<script>var slider = document.getElementById(\"servoSlider\");");
+            client.println("var servoP = document.getElementById(\"servoPos\"); servoP.innerHTML = slider.value;");
+            client.println("slider.oninput = function() { slider.value = this.value; servoP.innerHTML = this.value; }");
+            client.println("$.ajaxSetup({timeout:1000}); function servo(pos) { ");
+            client.println("$.get(\"/?value=\" + pos + \"&\"); {Connection: close};}</script>");
+           
+            client.println("</body></html>");     
+            
+            
+            if(header.indexOf("GET /?value=")>=0) {
+              pos1 = header.indexOf('=');
+              pos2 = header.indexOf('&');
+              valueString = header.substring(pos1+1, pos2);
+              
+              
+              myservo.write(valueString.toInt());
+              Serial.println(valueString); 
+            }         
+            
+            client.println();
+            
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    
+    header = "";
+    
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+}
